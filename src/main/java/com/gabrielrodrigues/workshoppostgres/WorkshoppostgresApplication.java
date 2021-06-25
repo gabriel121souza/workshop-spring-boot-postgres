04f6f69b666c69506075a0f71e6ee1a051722169
Gabriel de Souza <gabriel121souza@gmail.com>
2021-06-25 11:59:43 -0300

    projeto criado

---
+++ src/main/java/com/gabrielrodrigues/workshoppostgres/WorkshoppostgresApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielrodrigues.workshoppostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshoppostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshoppostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielrodrigues/workshoppostgres/WorkshoppostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielrodrigues.workshoppostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshoppostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
