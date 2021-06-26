006accfee9f232d61607917319498adc3c0cf4f4
Gabriel de Souza <gabriel121souza@gmail.com>
2021-06-25 23:39:11 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/gabrielrodrigues/workshoppostgres/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.gabrielrodrigues.workshoppostgres.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.gabrielrodrigues.workshoppostgres.domain.User;
+import com.gabrielrodrigues.workshoppostgres.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		User gabriel = new User(null, "Gabriel", "gabriel121souza@gmail.com");
+		User daniel = new User(null, "Daniel", "daniel@gmail.com");
+		User juliana = new User(null, "Juliana", "juliana@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(gabriel, daniel, juliana));
+	}
+
+}
